b1bd6464fbf14b74f67657a769eab8c8e92d8850
Johan Linder <johan@fmca.se>
2022-06-10 14:01:28 +0200

    [imlinder] Add mouse and nav layer

--- keyboards/preonic/keymaps/imlinder/keymap.c
+++ keyboards/preonic/keymaps/imlinder/keymap.c
@@ -25,6 +25,7 @@ enum preonic_layers {
   _NAV,
   _NUM,
   _ADJUST,
+  _MOUSE,
   _FN,
 };
 
@@ -39,6 +40,10 @@ enum preonic_keycodes {
 #define CTL_DOWN MT(MOD_RCTL, KC_DOWN)
 #define ALT_LEFT MT(MOD_RALT, KC_LEFT)
 #define CTL_ESC LCTL_T(KC_ESC)
+#define LT_SPC LT(_NUM, KC_SPC)
+#define LT_BSPC LT(_NAV, KC_BSPC)
+#define A_MS LT(_MOUSE, KC_A)
+#define SCLN_NAV LT(_NAV, KC_SCLN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -58,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_preonic_grid(
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,    KC_7,  KC_8,     KC_9,     KC_0,    _______,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,  KC_I,     KC_O,     KC_P,    KC_BSPC,
-  CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,  KC_K,     KC_L,     KC_SCLN, KC_QUOT,
+  CTL_ESC, A_MS,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,  KC_K,     KC_L,     SCLN_NAV, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,  KC_COMM,  KC_DOT,   KC_SLSH, RSFT_T(KC_ENT),
   MO(_FN), KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC, KC_BSPC, RAISE, ALT_LEFT, CTL_DOWN, KC_UP,   KC_RIGHT
 ),
@@ -159,13 +164,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______,    _______,    _______,    _______,     _______, _______
 ),
 
-/* [_ADJUST] = LAYOUT_preonic_grid( */
-/*   KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,      KC_F7,      KC_F8,      KC_F9,       KC_F10,  KC_F11, */
-/*   _______, _______, _______, _______, _______, _______, _______,    _______,    _______,    _______,     _______, KC_DEL, */
-/*   _______, _______, _______, _______, _______, _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP,   KC_MS_RIGHT, _______, _______, */
-/*   _______, _______, _______, _______, _______, _______, _______,    KC_MS_BTN1, KC_MS_BTN2, _______,     _______, _______, */
-/*   _______, _______, _______, _______, _______, _______, _______,    _______,    _______,    _______,     _______, _______ */
-/* ), */
+[_MOUSE] = LAYOUT_preonic_grid(
+   _______, _______, _______, _______, _______, _______, _______,    _______,    _______,    _______,     _______, _______,
+   _______, _______, _______, _______, _______, _______, _______, KC_ACL0, KC_ACL1, KC_ACL2,  KC_WH_D, _______,
+   _______, _______, _______, _______, _______, _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP,   KC_MS_RIGHT, _______, _______,
+   _______, _______, _______, _______, _______, _______, KC_WH_U,    _______,    _______,    _______,     _______, _______,
+   _______, _______, _______, _______, _______, KC_MS_BTN1, KC_MS_BTN2, _______,  _______,     _______, _______, _______
+),
 
 #define SWP_LFT MAGIC_SWAP_LALT_LGUI
 #define USWP_LFT MAGIC_UNSWAP_LALT_LGUI
